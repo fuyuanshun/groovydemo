fab75ad07501f6b2edc40a0d2bfcde53c528e655
fuyuanshun <fuyuanshun521@gmail.com>
2022-10-12 16:09:26 +0800

    测试类

--- src/test/groovy/com/fys/groovydemo/GroovydemoApplicationTests.groovy
+++ src/test/groovy/com/fys/groovydemo/GroovydemoApplicationTests.groovy
@@ -16,4 +16,24 @@ class GroovydemoApplicationTests {
 		addNum(3, 4)
 	}
 
+	/**
+	 * 引号测试
+	 */
+	@Test
+	void testQuotes(){
+		def s = "fys"
+
+		// 不会解析其中的变量$
+		// 结果： abc $s
+		println 'abc $s'
+
+		// 可以解析其中的$
+		// 结果： abc fys
+		println "abc $s"
+
+		// 不会解析其中的变量$ 但是可以解析格式
+		println '''abc $s 23
+			324235235
+		'''
+	}
 }
